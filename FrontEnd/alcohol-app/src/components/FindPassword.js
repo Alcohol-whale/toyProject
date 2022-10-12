e858a22ca330999867e2bd88620a27f3fa0cb3e0
Harimmm <88179702+Harimmm@users.noreply.github.com>
2022-10-12 19:58:43 +0900

    first

--- FrontEnd/alcohol-app/src/components/FindPassword.js
+++ FrontEnd/alcohol-app/src/components/FindPassword.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import Introheader from './Introheader';
+import Footer from './Footer';
+import '../assets/register.css'
 
 function FindPassword(){
     return(
-        <Link to="/findPassword">
-        </Link>
+        <div class='outline'>
+            <Introheader/>
+            <div class='form-wrap'>
+                <div class='id-box'>
+                <input type="text" class="form-control" placeholder="이름" aria-describedby="basic-addon2" style={{width:'200px', height:'50px'}}/>
+                </div>
+                <div class='id-box'>
+                <input type="text" class="form-control" placeholder="아이디@이메일" aria-describedby="basic-addon2" style={{width:'200px', height:'50px'}}/>
+                <button type="button" class="btn btn-primary" style={{쟝소:'100테', heigth:'20px'}}> 인증번호 받기 </button>
+                </div>
+                <div class='id-box'>
+                <input type="text" class="form-control" placeholder="인증 번호 입력" aria-describedby="basic-addon2" style={{width:'200px', height:'50px'}}/>
+                <button type="button" class="btn btn-primary" style={{heigth:'20px'}}> 인증</button>
+                </div>
+            </div>
+            <Footer/>
+        </div>
     )
 }
 
